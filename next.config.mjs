6bdc13a9f733bdc49cc6f87d02723bf2d67bdb30
Henryck <guaramatohenryck@gmail.com>
2026-02-13 16:20:45 -0300

    Eliminar configuración de webpack innecesaria para canvas en next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,6 @@ const nextConfig = {
     unoptimized: true,
   },
   serverExternalPackages: ["canvas", "pdfjs-dist"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.alias.canvas = false;
-    }
-    return config;
-  },
 }
 
 export default nextConfig
